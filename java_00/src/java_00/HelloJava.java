186b9ea2e7bddb8ae6bd317057cf4a353c225ff1
USER <USER@1.209.180.138>
2024-06-19 09:28:13 +0900

    <2024.06.19> - 최초 커밋

---
+++ java_00/src/java_00/HelloJava.java
@@ -0,0 +1,17 @@
+package java_00;
+
+public class HelloJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String test1 = "안녕하세용, 저는 임유진입니다.";
+
+				
+		System.out.println(test1);
+		
+		System.out.println("Hello Java 3 !!!");
+		
+	}	
+
+}
